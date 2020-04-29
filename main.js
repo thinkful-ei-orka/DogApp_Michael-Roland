a4d69b39ca9c47a17606707db2d194378ebb5e44
Michael Blalock <mblalock90@yahoo.com>
2020-04-29 14:57:12 -0400

    numbers 1 and 2

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 function handleSubmit() { $('#dogForm').on('click',
-    '#js-dogButton', event => { event.preventDefault();
-        const count = $("#dogInput").val();
-        console.log(count);
-    }) }
-    function displayResults(responseJson) { console.log(responseJson);
-        //replace the existing image with the new one
-        $('#dogImg').replaceWith( `<img src="${responseJson.message}" class="results-img">` )
-        //display the results section
-        $('.results').removeClass('hidden'); }
+  '#js-dogButton', event => { 
+    event.preventDefault();
+    let count = $('#dogInput').val();
+    for(count; count > 0; count--){
+        getDogImage();
+    }
+  });
+}
+function displayResults(responseJson) { 
+    console.log(Object.entries(responseJson));
+    //replace the existing image with the new one
+    $('#dogPhotos').append($(`<img src="${responseJson.message}" class="results-img">`));
+}
 
-
-function getDogImage() { fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage() { 
+    fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json()) 
 .then(responseJson => displayResults(responseJson))
 .catch(error => alert('Something went wrong. Try again later.')); }
 
-
-$(handleSubmit);
-$(getDogImage);
+function handleApp(){
+    handleSubmit();
+}
+$(handleApp);
